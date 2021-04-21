f3ca2e1a723a58a3cc629cb94cb0ac73cb206682
Nathan Souza <nathanpereirasouza@gmail.com>
2021-04-21 11:09:41 -0300

    configurando disponibilidade do mês

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,9 @@
 import { shade } from 'polished';
 import styled from 'styled-components';
 
+import ArrowLeftIcon from '../../assets/ArrowLeftIcon.svg';
+import ArrowRightIcon from '../../assets/ArrowRightIcon.svg';
+
 export const Container = styled.div``;
 
 export const Header = styled.div`
@@ -212,53 +215,101 @@ export const Calendar = styled.aside`
   width: 380px;
 
   .DayPicker {
-    background: #28262e;
     border-radius: 10px;
-  }
 
-  .DayPicker-wrapper {
-    padding-bottom: 0;
-  }
+    &-wrapper {
+      padding-bottom: 0;
+      background: #28262e;
+      border-radius: 10px;
+      z-index: 0;
+    }
 
-  .DayPicker,
-  .DayPicker-Month {
-    width: 100%;
-  }
+    &-NavBar {
+      position: relative;
 
-  .DayPicker-Month {
-    border-collapse: separate;
-    border-spacing: 8px;
-    margin: 16px;
-  }
+      ::before {
+        content: '';
+        width: 100%;
+        height: 50px;
+        position: absolute;
+        background: #3e3b47;
+        border-radius: 10px 10px 0 0;
+        z-index: -1;
+      }
+    }
 
-  .DayPicker-Day {
-    width: 40px;
-    height: 40px;
-  }
+    &-NavButton {
+      color: #999591 !important;
+      margin-top: 0;
+      top: 0;
 
-  .DayPicker-Day--available:not(.DayPicker-Day--outside) {
-    background: #3e3b47;
-    border-radius: 10px;
-    color: #fff;
-  }
+      &--prev {
+        background: url(${ArrowLeftIcon}) no-repeat center;
+        margin-right: 0;
+        left: 12px;
+        width: 50px;
+        height: 50px;
+      }
 
-  .DayPicker:not(.DayPicker--interactionDisabled)
-    .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
-    background: ${shade(0.2, '#3e3b47')};
-  }
+      &--next {
+        background: url(${ArrowRightIcon}) no-repeat center;
+        right: 12px;
+        width: 50px;
+        height: 50px;
+      }
+    }
 
-  .DayPicker-Day--today {
-    font-weight: normal;
-  }
+    &-Month {
+      border-collapse: separate;
+      border-spacing: 8px;
+      margin: 0;
+      padding: 0 10px 10px;
+    }
 
-  .DayPicker-Day--disabled {
-    color: #666360 !important;
-    background: transparent !important;
-  }
+    &-Caption {
+      line-height: 50px;
+      color: #f4ede8;
 
-  .DayPicker-Day--selected {
-    background: #ff9000 !important;
-    border-radius: 10px;
-    color: #232129 !important;
+      > div {
+        text-align: center;
+      }
+    }
+
+    &-Weekday {
+      color: #666360;
+      font-size: 16px;
+    }
+
+    &-Day {
+      width: 40px;
+      height: 40px;
+      transition: all 0.2s ease;
+      border-radius: 10px;
+
+      &--today {
+        font-weight: normal;
+        color: #fff;
+      }
+
+      &--available:not(.DayPicker-Day--outside) {
+        background: #3e3b47;
+        border-radius: 10px;
+      }
+
+      &--disabled {
+        color: #666360;
+        background: transparent !important;
+      }
+
+      &--selected:not(.DayPicker-Day--disabled) {
+        background: #ff9000 !important;
+        color: #232129 !important;
+      }
+    }
+
+    &:not(.DayPicker--interactionDisabled)
+      .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+      background: ${shade(0.2, '#3e3b47')};
+    }
   }
 `;
